1e83e8fba65c9165f6a5f458d54dcd3b2377dd2e
Tiago Tavares <tiagotvrs@gmail.com>
2025-04-05 19:00:25 -0300

    Review Structure

--- src/CocodriloStore.Core/Interfaces/ICategoriaRepositorio.cs
+++
@@ -1,8 +0,0 @@
-using CocodriloStore.Core.Models;
-
-namespace CocodriloStore.Core.Interfaces;
-
-public interface ICategoriaRepositorio : IRepositorio<Categoria>
-{
-    Task<bool> CategoriaPossuiProdutosAsync(Guid id);
-}
--- src/CocodriloStore.Core/Interfaces/IVendedorRepositorio.cs
+++
@@ -1,8 +0,0 @@
-using CocodriloStore.Core.Models;
-
-namespace CocodriloStore.Core.Interfaces;
-
-public interface IVendedorRepositorio : IRepositorio<Vendedor>
-{
-    Task<Vendedor> ObterVendedorPorUsuarioIdAsync(string usuarioId);
-}
